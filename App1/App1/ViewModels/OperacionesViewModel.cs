865cc2fbf5444a8d9e6a46b60113184c7cfc7233
sabrina alexandra pomajulca razabal <lokita_sabri_14@hotmail.com>
2019-10-21 16:17:04 -0500

    primer commit

---
+++ App1/App1/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class OperacionesViewModel : ViewModelBase
+    {
+
+        #region Propiedades
+        int valor1;
+        public int Valor1
+        {
+            get { return valor1; }
+            set
+            {
+                if (valor1 != value)
+                {
+                    valor1 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int valor2;
+        public int Valor2
+        {
+            get { return valor2; }
+            set
+            {
+                if (valor2 != value)
+                {
+                    valor2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int dividir;
+        public int Dividir
+        {
+            get { return dividir; }
+            set
+            {
+                if (dividir != value)
+                {
+                    dividir = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int multi;
+        public int Multi
+        {
+            get { return multi; }
+            set
+            {
+                if (multi != value)
+                {
+                    multi = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int suma;
+        public int Suma
+        {
+            get { return suma; }
+            set
+            {
+                if (suma != value)
+                {
+                    suma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resta;
+        public int Resta
+        {
+            get { return resta; }
+            set
+            {
+                if (resta != value)
+                {
+                    resta = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+
+        #region Comandos 
+        public ICommand Sumar { protected set; get; }
+
+        public ICommand Restar { protected set; get; }
+
+        public ICommand Division { protected set; get; }
+
+        public ICommand Multiplicacion { protected set; get; }
+        #endregion
+
+        #region Constructor
+
+        public OperacionesViewModel()
+        {
+
+            Sumar = new Command(() =>
+            {
+                Suma = Valor1 + Valor2;
+            });
+
+            Restar = new Command(() =>
+            {
+               Resta = Valor1 - Valor2;
+            });
+
+            Division = new Command(() =>
+            {
+                Dividir = Valor1 / Valor2;
+            });
+
+            Multiplicacion = new Command(() =>
+            {
+                Multi = Valor1  * Valor2;
+            });
+
+        }
+        #endregion
+
+    }
+}
+
---
+++ App1/App1/Views/Operaciones.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operaciones : ContentPage
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+            this.BindingContext = new OperacionesViewModel();
+        }
+    }
+
+}
